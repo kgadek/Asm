011381c0f161ae43dfaa5805b24a2d544a09c572
Konrad G <kgadek@gmail.com>
2010-11-08 06:17:11 +0100

    Poprawki kosmetyczne

--- ra.asm
+++ ra.asm
@@ -7,33 +7,32 @@ dane1	segment
 dane1	ends
 
 
-code1	segment
-start:
-		; === inicjowanie stosu
+code1	segment ;____________________________________________________________
+start:	; === inicjowanie stosu
 	mov	ax,seg top1		; segment stosu -> SS
 	mov	ss,ax
 	mov	sp,offset top1	; offset stosu -> SP
 
 		; === Wyświetlenie napisu
-	mov cl,es:80h		; CX=ilość bajtów argumentów
+	mov cl,es:80h				; CX=ilość bajtów argumentów
 	xor ch,ch
-	jcxz brak_arg		; jeśli CX==0 to skocz do brak_arg
-	sub cx,1
-	mov bx,82h
-	mov ah,2h			; wyświetlanie argumentów
-petla:					; WHILE(CX!=0) {
-	mov dl,es:[bx]
-	int 21h
-	add bx,1
-	loop petla			; }
-	jcxz fin
+	jcxz brak_arg				; if(CX != 0) {
+	sub cx,1					; 	CX--
+	mov bx,82h					; 	BX = 82h (offset argumentów)
+	mov ah,2h					; 	wyświetlanie argumentów
+petla:							; 	WHILE(CX!=0) {
+	mov dl,es:[bx]				;		DL przyjmuje kolejne znaki parametrów z wiersza poleceń
+	int 21h						;		wyświetl
+	add bx,1					;		zwiększ offset BX
+	loop petla					; 	}
+	jcxz fin					; } else {
 		; === Błąd: brak argumentu
-brak_arg:
-	mov ax, seg errBrakArg
+brak_arg:						;
+	mov ax, seg errBrakArg		; 	DS = segment komunikatu błędu
 	mov ds, ax
-	mov dx, offset errBrakArg
-	mov ah, 9
-	int 21h
+	mov dx, offset errBrakArg	;	DX = offset komunikatu błędu
+	mov ah, 9					;	wypisz komunikat o błędzie
+	int 21h						; }
 
 fin:	; === zakończ program
 	mov	ax,04c00h
@@ -41,7 +40,7 @@ fin:	; === zakończ program
 code1	ends
 
 
-stos1	segment STACK
+stos1	segment STACK ;______________________________________________________
 top1	dw ?
 stos1	ends
 
